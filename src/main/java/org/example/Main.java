ebe9ac9fb1bfeb07d0f15e001afc1fd8c113718c
Sreenath <sreenath.code@gmail.com>
2022-12-14 13:50:27 -0500

    Initial Changes

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    /**
+     * This is a test class for store. Please use this classes resposibly
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Sreenath Sasidharan");
+    }
+}
