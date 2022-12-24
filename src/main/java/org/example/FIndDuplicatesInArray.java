38849fad9ec86514b3050b4b0ce96c0bb7d8ab32
Sreenath <sreenath.code@gmail.com>
2022-12-24 01:47:26 -0500

    Revert

--- src/main/java/org/example/FIndDuplicatesInArray.java
+++ src/main/java/org/example/FIndDuplicatesInArray.java
@@ -1,54 +1,13 @@
 package org.example;
 
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
 
 public class FIndDuplicatesInArray {
     public static void main(String[] args) {
-//        isDuplicateInUnsortedArray();
-//        isDuplicateInSortedArray();
-       // findSum();
-        System.out.println(romanToInt("MCMXCIV"));
-        ;
+        isDuplicateInUnsortedArray();
+        isDuplicateInSortedArray();
     }
 
-    private static void findSum() {
-        int target = 6;
-        int arr[] = {3,2,4};
-        int k=0;
-        Arrays.sort(arr);
-        int[] output = new int[2];
-        int i = 0;
-        int j=  arr.length-1;
-        while(i<=j){
-            int sum = arr[i] + arr[j];
-            if(sum == target){
-                output[0] = i;
-                output[1] = j;
-                break;
-            }else if(sum > target){
-                j--;
-            }else{
-                i++;
-            }
-        }
-        for(int s: output){
-            System.out.println(s);
-        }
-
-      /*  for(int i=0; i < arr.length; i++) {
-            for(int j=i+1; j< arr.length; j++) {
-                if(arr[i]+arr[j] == target) {
-                    output[k] = arr[i];
-                    output[k+1] = arr[j];
-                }
-            }
-
-        }*/
-
-    }
     private static void isDuplicateInSortedArray() {
         boolean hasDuplicate = false;
         int[] arr = {1, 2, 2, 3, 4, 5};
@@ -64,63 +23,19 @@ public class FIndDuplicatesInArray {
     }
 
     private static void isDuplicateInUnsortedArray() {
-        int[] arr = {0,1, 2, 3, 4, 5, 1, 4 , 4};
-        boolean duplicate = false;
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i=0; i< arr.length; i++) {
+        int[] arr = {1, 2, 3, 4, 5, 1, 3, 4};
+        boolean hasDuplicate = false;
 
-            map.put(arr[i],map.getOrDefault(arr[i],0));
-        }
-        map.entrySet().stream().forEach(System.out::println);
-        /*boolean hasDuplicate = false;
-        int count = 0, k=0;
-        int[] output = new int[arr.length];
-
-        for(int i=0; i< arr.length; i++){
-            for(int j= i+1; j< arr.length; j++) {
-
-                if(arr[i]== arr[j]) {
-                    count+=1;
-                    output[count] = arr[i];
-                }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (map.containsKey(arr[i]) && map.get(arr[i]) > 0) {
+                hasDuplicate = true;
+                break;
+            } else {
+                map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
             }
         }
-        for(int m : output) {
-            System.out.println(m);
-        }*/
 
-
+        System.out.println(hasDuplicate);
     }
-    public static int romanToInt(String s) {
-        char[] r = s.toCharArray();
-        int sum = 0;
-        char last = 'J';
-        Map<Character, Integer> map = new HashMap<>();
-        map.put('I',1);
-        map.put('V',5);
-        map.put('X',10);
-        map.put('L',50);
-        map.put('C',100);
-        map.put('D',500);
-        map.put('M',1000);
-        for(int i= 0; i <r.length; i++){
-            if(s == null || s == ""){
-                return sum;
-            }
-            if(map.get(last) == null){
-                sum+=map.get(r[i]);
-                continue;
-            }
-            System.out.println(map.get(r[i]));
-            if(map.get(last) < map.get(r[i])){
-                sum-=map.get(r[i]);
-            }else{
-                sum+=map.get(r[i]);
-            }
-        }
-
-        return sum;
-
-    }
-
 }
